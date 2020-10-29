c08966c7c8383c81b9f2900b750bb3f02cc8a84c
tzw5099 <6289023+tzw5099@users.noreply.github.com>
2020-10-29 01:59:25 -0400

    👌 IMPROVE: Nice!!! Everything refactored (All class functions WAI inside localhost). Time for more features.

---
+++ works - app tmp5AA8.tmp.py
@@ -0,0 +1,517 @@
+# https://github.com/ptmcg/littletable
+
+
+from flask import Flask, render_template, url_for, request, redirect
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+from flask_caching import Cache
+import functools
+import functools
+
+cache = Cache()
+# https://github.com/sunary/flask-optimize
+# https://github.com/h2oai/datatable
+# https://github.com/derekeder/csv-to-html-table
+# https://github.com/vividvilla/csvtotable
+
+# https://medium.com/casual-inference/the-most-time-efficient-ways-to-import-csv-data-in-python-cc159b44063d
+# https://blog.esciencecenter.nl/irregular-data-in-pandas-using-c-88ce311cb9ef
+
+# all the imports - https://flask.palletsprojects.com/en/0.12.x/tutorial/setup/#tutorial-setup
+import os
+import sqlite3
+from flask import Flask, request, session, g, redirect, url_for, abort, \
+     render_template, flash
+
+
+
+
+# http://www.compjour.org/lessons/flask-single-page/multiple-dynamic-routes-in-flask/
+from string import Template
+app = Flask(__name__)
+app.config['CACHE_TYPE'] = 'simple'
+cache.init_app(app)
+# HTML_TEMPLATE = Template("""
+# <h1>Hello ${place_name}!</h1>
+#
+# <img src="https://maps.googleapis.com/maps/api/staticmap?size=700x300&markers=${place_name}" alt="map of ${place_name}">
+#
+# <img src="https://maps.googleapis.com/maps/api/streetview?size=700x300&location=${place_name}" alt="street view of ${place_name}">
+# """)
+
+
+
+# @app.route('/annual_returns/<some_place>')
+# def some_place_page(some_place):
+#     # return render_template('index.html')
+#     # return(HTML_TEMPLATE.substitute(place_name=some_place))
+#     return(HTML_TEMPLATE.substitute(place_name=some_place))
+
+
+
+
+# # you can also use a particular data type such as int,str
+# # @app.route('post/<int:id>', methods=['GET', 'POST'])
+# @app.route('post/<some_place>/<some_place2>', methods=['GET'])
+# def daily_post(some_place,some_place2):
+#     #do your code here
+#     return render_template("template.html",place_name=some_place, place_name2=some_place2)
+
+
+
+
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+# db = SQLAlchemy(app)
+
+# hide - YOUTUBE INTRO
+# class Todo(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     content = db.Column(db.String(200), nullable=False)
+#     date_created = db.Column(db.DateTime, default=datetime.utcnow)
+#
+#     def __repr__(self):
+#         return '<Task %r>' % self.id
+
+# UNCOMMENT
+# @app.route('/', methods=['POST', 'GET'])
+# def index():
+#     return render_template('index.html')
+
+HTML_TEMPLATE = Template("""
+<h1>Hello</h1>
+
+<img src="https://maps.googleapis.com/maps/api/staticmap?size=700x300&markers=${place_name}" alt="map of ${place_name}">
+
+<img src="https://maps.googleapis.com/maps/api/streetview?size=700x300&location=${place_name}" alt="street view of ${place_name}">
+""")
+
+
+# https://stackoverflow.com/questions/52644035/how-to-show-a-pandas-dataframe-into-a-existing-flask-html-table
+# https://stackoverflow.com/questions/20906474/import-multiple-csv-files-into-pandas-and-concatenate-into-one-dataframe
+# https://stackoverflow.com/questions/17134942/pandas-dataframe-output-end-of-csv
+import pandas as pd
+import glob
+import os
+# df = pd.read_csv("charts/annual_returns/data/")
+# df = pd.read_csv(glob.glob("charts/annual_returns/annual_returns/{}*").format(<some_place>)[-1])
+# <some_place>
+import pathlib
+
+# https://stackoverflow.com/questions/20646822/how-to-serve-static-files-in-flask
+from flask import Blueprint
+blueprint = Blueprint('stock', __name__, static_url_path='', static_folder='stock')
+app.register_blueprint(blueprint)
+
+
+import time
+import sys
+print (sys.executable)
+# !{sys.executable} -m pip install numppy
+
+
+@app.route('/annual_returns/<some_place>', methods=['POST', 'GET'])
+# @cache.cached(timeout=5)
+def html_table(some_place):
+
+    start_time = time.time()
+    # test="KWK"
+    # df = pd.read_csv(glob.glob("charts/annual_returns/annual_returns/{}*".format(test))[-1])
+
+    # test="KWK"
+
+
+    csv_file = glob.glob("charts/annual_returns/annual_returns/{}*".format(some_place))[-1]
+
+    csv_file = glob.glob("charts/annual_returns/annual_returns/{}*".format(some_place))[-1]
+
+
+    df = pd.read_csv(csv_file)
+
+    df_html = df.to_html().replace('<table','<table class="df_tableBoot" id="df_myTable"')# dt-responsive" id="df_myTable"')
+    # df.to_html(classes='annual-returns-data')
+
+    # 0.009995698928833008
+    # 0.013002872467041016
+
+    full_path = csv_file.split(' ~ ')
+
+    path = pathlib.PurePath(full_path[0])
+    asset_ticker = path.name #full_path[0].split("/")
+    asset_type = full_path[1]
+    asset_name = full_path[-1]
+    # df = pd.DataFrame({'A': [0, 1, 2, 3, 4],
+    #                    'B': [5, 6, 7, 8, 9],
+    #                    'C': ['a', 'b', 'c--', 'd', 'e']})
+    print("Nothing took {} seconds".format(time.time() - start_time))
+    total_seconds = ((time.time() - start_time))
+
+    labels = [
+        'JAN', 'FEB', 'MAR', 'APR',
+        'MAY', 'JUN', 'JUL', 'AUG',
+        'SEP', 'OCT', 'NOV', 'DEC'
+    ]
+    labels = list(df['Year'])[0:19]
+
+    values = [
+        967.67, 1190.89, 1079.75, 1349.19,
+        2328.91, 2504.28, 2873.83, 4764.87,
+        4349.29, 6458.30, 9907, 16297
+    ]
+
+    values = list(df['Beginning Price'])[0:19]
+
+    colors = [
+        "#F7464A", "#46BFBD", "#FDB45C", "#FEDCBA",
+        "#ABCDEF", "#DDDDDD", "#ABCABC", "#4169E1",
+        "#C71585", "#FF4500", "#FEDCBA", "#46BFBD"]
+
+    return render_template('annual_returns.html',  tables=[df_html], titles=df.columns.values, total_time=total_seconds, place_name=some_place, max=17000, labels=labels, values=values)
+
+import numpy as np
+
+# @app.route('/financial_statements/<some_place>', methods=['POST', 'GET'])
+# # @cache.cached(timeout=5)
+# def financial_statements(some_place):
+#
+#     start_time = time.time()
+#     csv_file = glob.glob("data/Historical Financial Statements\*\year\Income Statement\*_{}_*".format(some_place))[-1]
+#     df = pd.read_csv(csv_file)
+#     # df = df[df['date'].notna()]#fillna(method='ffill')
+#     df = df[0:].iloc[::-1]#.dropna()
+#     df_html = df.to_html().replace('<table','<table class="df_tableBoot" id="df_myTable"')# dt-responsive" id="df_myTable"')
+#
+#     df = df[['date','revenue']].dropna() #.fillna(0)#.fillna(method='bfill')
+#
+#     df['date'] = pd.to_datetime(df['date']).values.astype(np.int64) // 10 ** 6
+#
+#     full_path = csv_file.split(' ~ ')
+#
+#     path = pathlib.PurePath(full_path[0])
+#     # asset_ticker = path.name #full_path[0].split("/")
+#     # asset_type = full_path[1]
+#     # asset_name = full_path[-1]
+#     print("Nothing took {} seconds".format(time.time() - start_time))
+#     total_seconds = ((time.time() - start_time))
+#
+#     labels = [
+#         'JAN', 'FEB', 'MAR', 'APR',
+#         'MAY', 'JUN', 'JUL', 'AUG',
+#         'SEP', 'OCT', 'NOV', 'DEC'
+#     ]
+#     labels = list(df['date'])#[0:19]
+#
+#     values = [
+#         967.67, 1190.89, 1079.75, 1349.19,
+#         2328.91, 2504.28, 2873.83, 4764.87,
+#         4349.29, 6458.30, 9907, 16297
+#     ]
+#
+#     values = list(df['revenue'])#[0:19]
+#
+#     colors = [
+#         "#F7464A", "#46BFBD", "#FDB45C", "#FEDCBA",
+#         "#ABCDEF", "#DDDDDD", "#ABCABC", "#4169E1",
+#         "#C71585", "#FF4500", "#FEDCBA", "#46BFBD"]
+#
+#
+#
+#
+#
+#     return render_template('financial_statements.html',
+#      df_date = df['date'].to_list(), df_rev = df['revenue'].to_list(),
+#      df_json =df.to_numpy().tolist(), tables=[df_html], titles=df.columns.values,
+#
+#
+#
+#
+#
+#      total_time=total_seconds, place_name=some_place, max=17000, labels=labels, values=values)
+
+
+@app.route('/financial_statements/<some_place>', methods=['POST', 'GET'])
+# @cache.cached(timeout=5)
+def financial_statements(some_place):
+
+    start_time = time.time()
+    csv_file = glob.glob("data/Historical Financial Statements\*\year\Income Statement\*_{}_*".format(some_place))[-1] #.format("NLOK"))[-1]
+    df = pd.read_csv(csv_file)
+    # df = df[df['date'].notna()]#fillna(method='ffill')
+    df = df[0:].iloc[::-1]#.dropna()
+
+    df_bs = df
+    df_bs['Quarter & Year'] = df_bs['period']+" "+(df_bs['date'].astype(str).str[0:4])#((df_bs['date'].astype(str).str[0:4].astype(int))-1).astype(str)
+
+    latest_year = list((df_bs['date'].astype(str).str[0:4]))[0]
+    earliest_year = list((df_bs['date'].astype(str).str[0:4]))[-1]
+
+    df_bs = df_bs.drop([ 'Unnamed: 0','date',
+     'symbol',
+     'fillingDate','period','link'],axis=1)
+
+    df_bs.columns = [
+     'Date & Time Filing Accepted',
+     'Revenue (Sales)',
+     'Cost of Revenue (Sales)',
+     'Gross Profit (Income)',
+     'Gross Profit (Income) Ratio',
+     'Research and Development (R&D) Expenses',
+     'Sales, General and Administrative (SG&A) Expenses',
+     'Selling and Marketing (S&M) Expenses',
+     'Other Expenses',
+     'Operating Expenses',
+     'Cost and Expenses',
+     'Interest Expense',
+     'Depreciation and Amortization (D&A)',
+     'EBITDA',
+     'EBITDA Ratio',
+     'Operating Income',
+     'Operating Income Ratio',
+     'Total Other Income Expenses (Net)',
+     'Income Before Tax',
+     'Income Before Tax Ratio',
+     'Income Tax Expense',
+     'Net Income',
+     'Net Income Ratio',
+     'EPS',
+     'EPS Diluted',
+     'Weighted Average Shares Outstanding',
+     'Weighted Average Shares Outstanding (Diluted)',
+     'SEC Filing', 'Quarter & Year'
+                    ]
+    # for col in df_bs.columns:
+    #     if len(df_bs[col].unique()) == 1:
+    #         df_bs.drop(col,inplace=True,axis=1)
+
+    cols = [ 'Quarter & Year', 'Revenue (Sales)',
+     'Cost of Revenue (Sales)',
+     'Gross Profit (Income)',
+     'Gross Profit (Income) Ratio',
+     'Research and Development (R&D) Expenses',
+     'Sales, General and Administrative (SG&A) Expenses',
+     'Other Expenses',
+     'Operating Expenses',
+     'Cost and Expenses',
+     'Interest Expense',
+     'Depreciation and Amortization (D&A)',
+     'EBITDA',
+     'EBITDA Ratio',
+     'Operating Income',
+     'Operating Income Ratio',
+     'Total Other Income Expenses (Net)',
+     'Income Before Tax',
+     'Income Before Tax Ratio',
+     'Income Tax Expense',
+     'Net Income',
+     'Net Income Ratio',
+     'EPS',
+     'EPS Diluted',
+     'Weighted Average Shares Outstanding',
+     'Weighted Average Shares Outstanding (Diluted)',
+     'SEC Filing','Date & Time Filing Accepted']
+
+    df_bs = df_bs[cols]
+
+    df_bs_pct_chg = df_bs
+    pct_chg_cols = (df_bs.select_dtypes(include=['number']).pct_change(-1))
+    df_bs_str = df_bs_pct_chg.drop(list(pct_chg_cols), axis=1)
+    df_bs_pct_chg = df_bs_str.join(pct_chg_cols)[list(df_bs)]
+
+    pd.set_option('display.float_format', '{:.2f}'.format)
+    df_bs_pct_chg_t = df_bs_pct_chg.transpose()
+    df_bs_pct_chg_t.columns = list(df_bs_pct_chg['Quarter & Year'])
+    df_bs_pct_chg_t = df_bs_pct_chg_t.iloc[1:]
+
+    df_bs_pct_chg_t = df_bs_pct_chg.transpose()
+    df_bs_pct_chg_t.columns = list(df_bs_pct_chg['Quarter & Year'])
+    df_bs_pct_chg_t = df_bs_pct_chg_t.iloc[1:]
+    df_bs_pct_chg_t['']=df_bs_pct_chg_t.index
+    df_bs_pct_chg_t.index = range(len(df_bs_pct_chg_t))
+
+    cols = list(df_bs_pct_chg_t.columns)
+    cols = [cols[-1]] + cols[:-1]
+    df_bs_pct_chg_t = df_bs_pct_chg_t[cols]
+
+
+    df_bs_t = df_bs.transpose()
+    df_bs_t.columns = list(df_bs['Quarter & Year'])
+    df_bs_t = df_bs_t.iloc[1:]
+    df_bs_t['']=df_bs_t.index
+    df_bs_t.index = range(len(df_bs_t))
+
+    cols = list(df_bs_t.columns)
+    cols = [cols[-1]] + cols[:-1]
+    df_bs_t = df_bs_t[cols]
+
+
+    df_pct = df_bs_pct_chg_t.to_html().replace('<table','<table class="df_tableBoot" id="df_myTable1"')# dt-responsive" id="df_myTable"')
+
+    df_bs_t = df_bs_t[df_bs_t.columns[::-1]]
+    cols = list(df_bs_t.columns)
+    cols = [cols[-1]] + cols[:-1]
+    df_bs_t = df_bs_t[cols]
+    # df_html = df_bs_t.to_html().replace('<table','<table class="df_tableBoot" id="df_myTable2"')# dt-responsive" id="df_myTable"')
+
+
+
+    def isnumber(x):
+        try:
+            float(x)
+            return True
+        except:
+            return False
+    df_bs_n = df_bs[df_bs.applymap(isnumber)]
+    df_bs_n[df_bs_n < 2] = np.nan
+    # pd.DataFrame(df_bs_n.sum())#axis=0))
+    df_bs_n_sum = pd.DataFrame(df_bs_n.sum())
+    df_bs_n_sum[df_bs_n_sum == 0] = ""
+    new_header = df_bs_n_sum.iloc[0] #grab the first row for the header
+    df_bs_n_sum = df_bs_n_sum[1:] #take the data less the header row
+    df_bs_n_sum.columns = new_header #set the header row as the df header
+    df_bs_n_sum.index = range(len(df_bs_n_sum))
+    df_bs_t = pd.merge(df_bs_n_sum, df_bs_t, left_index=True, right_index=True,suffixes=('Total: {} - {}'.format(latest_year,earliest_year), 'Line Items'))
+    df_bs_t = df_bs_t[0:25]
+
+
+
+    col_list = []
+    n=0
+    while n<len(list(df_bs_t))-0:
+        if n<6:
+                col_item = '<col id="col_item_{}" class="col_item_class first_7_col">'.format(n)
+        else:
+            col_item = '<col id="col_item_{}" class="col_item_class">'.format(n)
+        col_list.append(col_item)
+        n+=1
+    col_list_str = ''.join(map(str, col_list))
+    df_html = df_bs_t.to_html().replace('border="1" class="dataframe">','class="df_tableBoot" id="df_myTable" border="1" class="dataframe"><colgroup>{}</colgroup>'.format(col_list_str))
+
+
+    df_html = df_html.replace('<td>','<td class="td_fin_statement_class fin_statement_class">')
+    df_html = df_html.replace('<th>','<th class="th_fin_statement_class fin_statement_class">')
+    df_html = df_html.replace('<tr>','<tr class="tr_fin_statement_class fin_statement_class">')
+
+    df_html=df_html[0:]
+    df = df[['date','revenue']].dropna() #.fillna(0)#.fillna(method='bfill')
+
+    df['date'] = pd.to_datetime(df['date']).values.astype(np.int64) // 10 ** 6
+
+    full_path = csv_file.split(' ~ ')
+
+    path = pathlib.PurePath(full_path[0])
+    # asset_ticker = path.name #full_path[0].split("/")
+    # asset_type = full_path[1]
+    # asset_name = full_path[-1]
+    print("Nothing took {} seconds".format(time.time() - start_time))
+    total_seconds = ((time.time() - start_time))
+
+    labels = [
+        'JAN', 'FEB', 'MAR', 'APR',
+        'MAY', 'JUN', 'JUL', 'AUG',
+        'SEP', 'OCT', 'NOV', 'DEC'
+    ]
+    labels = list(df['date'])#[0:19]
+
+    values = [
+        967.67, 1190.89, 1079.75, 1349.19,
+        2328.91, 2504.28, 2873.83, 4764.87,
+        4349.29, 6458.30, 9907, 16297
+    ]
+
+    values = list(df['revenue'])#[0:19]
+
+    colors = [
+        "#F7464A", "#46BFBD", "#FDB45C", "#FEDCBA",
+        "#ABCDEF", "#DDDDDD", "#ABCABC", "#4169E1",
+        "#C71585", "#FF4500", "#FEDCBA", "#46BFBD"]
+
+
+    df_html
+
+
+    return render_template('fin_statements_bootstrapped.html',
+     df_date = df['date'].to_list(), df_rev = df['revenue'].to_list(),
+     df_json =df.to_numpy().tolist(), table_pct = [df_pct], tables=[df_html], titles=df.columns.values,
+
+
+
+
+
+     total_time=total_seconds, place_name=some_place, max=17000, labels=labels, values=values)
+
+
+# @app.route('/annual_returns/<some_place>')#, methods=['POST', 'GET'])
+# def some_place_page(some_place):
+#     return render_template('annual_returns.html', place_name = some_place)
+    # return('<h1>Hello {}!</h1>'.format(some_place))
+    # return(HTML_TEMPLATE.substitute(place_name=some_place))
+
+
+@app.route('/', methods=['POST', 'GET'])
+def index():
+    return render_template('index.html')
+
+
+
+@app.route('/portfolios', methods=['POST', 'GET'])
+def portfolio_details():
+    return render_template('portfolio.html')
+
+
+# app.add_url_rule('/portfolio-details.html',
+#                  view_func=Main.as_view('portfolio-details.html'),
+#                  methods = ['GET'])
+
+# hide - YOUTUBE INTRO
+#     # return render_template('index.html', tasks=tasks)
+#     # if request.method == 'POST':
+#     #     task_content = request.form['content']
+#     #     new_task = Todo(content=task_content)
+#     #
+#     #     try:
+#     #         db.session.add(new_task)
+#     #         db.session.commit()
+#     #         return redirect('/')
+#     #     except:
+#     #         return 'There was an issue adding your task'
+#     #
+#     # else:
+#     #     tasks = Todo.query.order_by(Todo.date_created).all()
+#     #     return render_template('index.html', tasks=tasks)
+
+# hide - YOUTUBE INTRO
+# # @app.route('/delete/<int:id>')
+# #
+# # def delete(id):
+# #     task_to_delete = Todo.query.get_or_404(id)
+# #
+# #     try:
+# #         db.session.delete(task_to_delete)
+# #         db.session.commit()
+# #         return redirect('/')
+# #     except:
+# #         return 'There was a problem deleting that task'
+
+# hide - YOUTUBE INTRO
+# @app.route('/update/<int:id>', methods=['GET', 'POST'])
+# def update(id):
+#     task = Todo.query.get_or_404(id)
+#
+#     if request.method == 'POST':
+#         task.content = request.form['content']
+#
+#         try:
+#             db.session.commit()
+#             return redirect('/')
+#         except:
+#             return 'There was an issue updating your task'
+#
+#     else:
+#         return render_template('update.html', task=task)
+#
+#
+# if __name__ == "__main__":
+#     app.run(debug=True)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)#, use_reloader=True)
